4f4e75af7b43eeb9ca5ca3381db2e53afcb32c68
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-07-16 16:35:45 +0300

    Tehtävä 3.4.

--- index.js
+++ index.js
@@ -51,6 +51,13 @@ const info_text =
         response.status(404).end();
     }
   })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
