091552780ff1868c7a8f4ab2c092b8ec042b3d94
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-07-16 16:57:55 +0300

    Tehtävä 3.5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 
 let persons = [
   {
@@ -33,14 +34,33 @@ const info_text =
     <p> ${now} </p>
 </div>`;
 
+// generate next id in order (not in use)
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(p => Number(p.id)))
+      : 0
+    return String(maxId + 1)
+  }
+
+  // random id generator
+  function randomId(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  }
+  
+
+  // get info 
   app.get('/info', (request, response) => {
     response.send(info_text)
   })
   
+  // get all persons
   app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+  // get person with id
   app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
     const person = persons.find(person => person.id === id)
@@ -52,12 +72,35 @@ const info_text =
     }
   })
 
+  // delete person with id
   app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
   })
+
+  // add person
+  app.post('/api/persons', (request, response) => {  
+    const body = request.body
+    console.log(body)  
+
+    if (!body.content) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }
+    
+    const person = {
+        id: randomId(person_amount, 999),
+        name: body.content.name,
+        number: body.content.number        
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
   
   const PORT = 3001
   app.listen(PORT, () => {
