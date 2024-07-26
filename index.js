5b7675efecc48e6b4ff87a28594136a13d461e45
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-07-26 14:27:14 +0300

    Tehtävä 3.11. full stack nettiin

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ morgan.token("show-content", function(req,res){
 //app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :show-content'));
 app.use(cors());
+app.use(express.static('dist'));
 
 let persons = [
   {
