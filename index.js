a651fbc2e739b3b60e77836f9b1a65b42abf4ea6
Kasper Kivistö <kivisto.kasper@gmail.com>
2024-07-26 14:00:07 +0300

    port muutos

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ const generateId = () => {
     response.status(201).json(new_person)
 })
   
-  const PORT = 3001
+const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
